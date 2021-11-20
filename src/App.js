ae5ac7a1da96d77b043b35aac1496b0f0b6c3528
17sushmita <17sushmita@gmail.com>
2021-11-20 23:36:33 +0530

    Coming soon page added

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+      <section> <span><img src="edfoal.png" alt="edfoal" height="100%" width="100%"/></span>
+        <div style={{padding:"20px"}}>
+          <h1>EDFOAL</h1>
+          <p>GREAT THINGS ARE COMING !!</p>
+        </div>
+      </section>
   );
 };
 
