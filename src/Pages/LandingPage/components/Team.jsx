c25770f1f50f3c9f4afc26442d1e2018678accae
Barun Debnath <barundebnath91@gmail.com>
2021-12-21 17:50:37 +0530

    Team component done

--- src/Pages/LandingPage/components/Team.jsx
+++ src/Pages/LandingPage/components/Team.jsx
@@ -4,7 +4,7 @@ export const Team = () => {
   return (
     <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
       <div className="mx-auto mb-10 lg:max-w-xl sm:text-center">
-        <p className="inline-block px-3 py-px mb-4 text-4xl	 font-semibold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
+        <p className="inline-block px-3 py-px mb-4 text-4xl	 font-bold tracking-wider text-teal-900 uppercase rounded-full bg-teal-accent-400">
           Know Our Team
         </p>
         <p className="text-base text-gray-700 md:text-lg">
@@ -143,6 +143,21 @@ export const Team = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      <div className="text-center py-9">
+        <a
+          href="/"
+          aria-label=""
+          className="inline-flex items-center font-semibold transition-colors duration-200 text-deep-purple-accent-400 hover:text-deep-purple-800">
+          View more
+          <svg
+            className="inline-block w-3 ml-2"
+            fill="currentColor"
+            viewBox="0 0 12 12">
+            <path d="M9.707,5.293l-5-5A1,1,0,0,0,3.293,1.707L7.586,6,3.293,10.293a1,1,0,1,0,1.414,1.414l5-5A1,1,0,0,0,9.707,5.293Z" />
+          </svg>
+        </a>
       </div>
     </div>
   );
